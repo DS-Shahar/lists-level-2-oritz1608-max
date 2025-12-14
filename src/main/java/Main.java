6f6c67ece6ab4a219c4eda8eec351b9745dd49ea
oritz1608-max <oritz1608@gmail.com>
2025-12-14 10:56:02 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+public static Node<Integer> L2EX3(Node<Integer> head, int x)  
+  {
+      Node<Integer> h = new Node<>(head.getValue(), head.getNext());
+      boolean boo =true;
+      int count =0;
+      int fCount=0;
+      while(boo==true)
+      {
+          if(h.getValue()==x)
+          {
+              boo =false;
+          }
+          else
+          {
+              count++;
+              h=h.getNext();
+          }
+      }
+      while(h.getValue()!=null)
+      {
+          h=h.getNext();
+          fCount++;
+          if(h.getValue==getNext)
+          {
+              fCount=0;
+          }
+      }
+      return count+fCount;
   }
-}
